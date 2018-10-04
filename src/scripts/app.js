6a0d4bedbbc575c31f6bf99d2cbdff5e8e1ac0fd
Kera <kera.andrea@gmail.com>
2018-10-04 15:51:51 -0400

    initial commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,56 @@
-const hello = 'World';
-console.log(hello);
+$(function() {
+    // on click check if the required field are valid
+    function testEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    function validateParentEmail() {
+        if (testEmail(document.getElementById('parentEmail').value)) {
+            return true;
+        } else {
+            alert("email not valid");
+            return false;
+        }
+    }
+
+    function validateDaycareEmail() {
+        if (testEmail(document.getElementById('daycareEmail').value)) {
+            return true;
+        } else {
+            alert("email not valid");
+            return false;
+        }
+    }
+
+  $('.letsTalk').on('click', function(e) {
+    e.preventDefault();
+    if ( $('.parentName').val() === ""
+    || $('.parentEmail').val() === ""
+    || $('.childName').val() === ""
+    || $('.daycareName').val() === ""
+    || $('.daycareEmail').val() === ""
+    || $('.daycareContact').val() === ""
+    || $('.phoneNumber').val() === ""
+    || validateDaycareEmail() === false
+    || validateParentEmail() === false) {
+        alert('Please fill out all of the required fields');
+        return false;
+    } else {
+        location.href = '/thankYou.html' 
+
+    }  
+  });   
+
+    //hamburger menu functionality
+    $('.hamburger').on('click', function () {
+        $('.menu').fadeIn("slow");
+    });
+    $('.exit').on('click', function () {
+        $('.menu').fadeOut("slow");
+    });
+    $('.navMobile.a').on('click', function () {
+        $('.menu').fadeOut("slow");
+    });    
+});
+
